432ff384f6e1adb0dccbdf91ac423b6da36a7d56
darshil2356 <darshilbavishi2356@gmail.com>
2025-09-11 00:39:47 +0530

    remove cercel

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ const corsOptions = {
       callback(new Error('Not allowed by CORS'));
     }
   },
+  credentials: true,
   methods: ['GET','POST','PUT','DELETE','OPTIONS'],
   allowedHeaders: ['Content-Type','Authorization']
 };
