ca1a2b5c83366af53c4c21c777c7f95dd027e5d4
darshil2356 <darshilbavishi2356@gmail.com>
2025-09-10 23:21:34 +0530

    resolve error

--- server.js
+++ server.js
@@ -15,11 +15,13 @@ const PORT = process.env.PORT || 4000;
 app.use(helmet());
 app.use(express.json({ limit: '10kb' })); 
 const corsOptions = {
-  origin: 'https://blog-59upu9doj-darshil2356s-projects.vercel.app', // allow your frontend
+  origin: 'http://localhost:3000',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
 app.use(cors(corsOptions));
+// Preflight for all routes
+app.options('*', cors(corsOptions));
 app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
 
 // routes
